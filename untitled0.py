1d98ed0599646dd7bdbe334a036f90fb261422b5
AlRhonemus <132271136+Acerpol@users.noreply.github.com>
2025-01-31 12:28:20 -0500

    initial commit

---
+++ untitled0.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 12:31:26 2025
+
+@author: alexa
+"""
+
+A = [[30, 1, 0],
+    [50, 2, 1],
+    [90,99, 100]]
+
+
+
+
+for row in A:
+    for collum in row:
+        print("A[" +str(row) +"]=")
+        print()
+        print(collum, end = " ")
+    print()
+    
+#============
+for Myindex_R, row in enumerate(A):
+    
+    
+        
